a599d3b32129b2dd16401db9d57a89f656e7da2f
Javier <j.perezurrutia@gmail.com>
2023-05-03 19:31:12 -0400

    fix utilidades

--- src/Utilidades.java
+++ src/Utilidades.java
@@ -1,49 +1,48 @@
 
-
-
 import java.util.Scanner;
 
 public class Utilidades {
-    public static String ValidaVacio(String str, String msj){
-        Scanner sc = new Scanner(System.in);
+    static Scanner sc = new Scanner(System.in);
+
+    public static String ValidaVacio(String str, String msj) {
+
         do {
-            System.out.println("Ingrese " + msj +":");
-            str=sc.nextLine();
-            if (str.isEmpty()){
-                System.out.println("[ ** WARNING ** ] Campo Obligatorio para "+ msj);
+            System.out.println("Ingrese " + msj + ":");
+            str = sc.nextLine();
+            if (str.isEmpty()) {
+                System.out.println("[ ** WARNING ** ] Campo Obligatorio para " + msj);
             }
-        }while(str.isEmpty());
+        } while (str.isEmpty());
         return str;
     }
 
-//nombre=ValidaVacio(nombre,"Nombres");
-    
+    // nombre=ValidaVacio(nombre,"Nombres");
 
-public static int ValidaNumero(int num, String msj){
-    Scanner sc = new Scanner(System.in);
-    boolean esNumero = false;
+    public static int ValidaNumero(int num, String msj) {
 
-    do {
-        System.out.println("Ingrese " + msj + ":");
-        if (sc.hasNextInt()){
-            num=sc.nextInt();
-            esNumero=true;
-        }else{
-            System.out.println("[ ** WARNING ** ] Valor ingresado  es invalido para "+ msj);
-            sc.next();
-            num=1;
-        }
-        if (num<1){
-            System.out.println("[ ** WARNING ** ] Valor debe ser superior a "+ num);
-            esNumero=false;
-        }
-    }while (!esNumero);
-     return num;
- }
+        boolean esNumero = false;
 
-}
+        do {
+            System.out.println("Ingrese " + msj + ":");
+            if (sc.hasNextInt()) {
+                num = sc.nextInt();
+                esNumero = true;
+            } else {
+                System.out.println("[ ** WARNING ** ] Valor ingresado  es invalido para " + msj);
+                sc.next();
+                num = 1;
+            }
+            if (num < 1) {
+                System.out.println("[ ** WARNING ** ] Valor debe ser superior a " + num);
+                esNumero = false;
+            }
+        } while (!esNumero);
+        return num;
+    }
 
-public int descomponerRut() {
+    public int descomponerRut(String rut) {
         String[] parts = rut.split("-");
         return Integer.parseInt(parts[0]);
     }
+
+}
