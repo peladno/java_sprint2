818d327065e384606c97d328520a95b0fae342b3
Claudio Castro Cifuentes <128555900+Claucas90@users.noreply.github.com>
2023-05-03 19:42:55 -0400

    realice un par de clases en la mañana ojala sirvan

--- src/Cliente.java
+++ src/Cliente.java
@@ -1,7 +1,106 @@
-public class Cliente extends Usuario {
-
-
-
-
-
+public class Cliente extends Usuario implements Asesoría {
+    
+    private int rut;
+    private String nombres;
+    private String apellidos;
+    private String telefono;
+    private String afp;
+    private int sistemaSalud;
+    private String direccion;
+    private String comuna;
+    private int edad;
+    
+    public Cliente(int rut, String nombres, String apellidos, String telefono, String afp, int sistemaSalud, String direccion, String comuna, int edad) {
+        this.rut = rut;
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.telefono = telefono;
+        this.afp = afp;
+        this.sistemaSalud = sistemaSalud;
+        this.direccion = direccion;
+        this.comuna = comuna;
+        this.edad = edad;
+    }
+    
+    public int getRut() {
+        return rut;
+    }
+    
+    public void setRut(int rut) {
+        this.rut = rut;
+    }
+    
+    public String getNombres() {
+        return nombres;
+    }
+    
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+    
+    public String getApellidos() {
+        return apellidos;
+    }
+    
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+    
+    public String getTelefono() {
+        return telefono;
+    }
+    
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    
+    public String getAfp() {
+        return afp;
+    }
+    
+    public void setAfp(String afp) {
+        this.afp = afp;
+    }
+    
+    public int getSistemaSalud() {
+        return sistemaSalud;
+    }
+    
+    public void setSistemaSalud(int sistemaSalud) {
+        this.sistemaSalud = sistemaSalud;
+    }
+    
+    public String getDireccion() {
+        return direccion;
+    }
+    
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    
+    public String getComuna() {
+        return comuna;
+    }
+    
+    public void setComuna(String comuna) {
+        this.comuna = comuna;
+    }
+    
+    public int getEdad() {
+        return edad;
+    }
+    
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    @Override
+    public void mostrarEdad() {
+        System.out.println("El usuario tiene " + this.edad + " años");
+    }
+    
+    @Override
+    public void analizarUsuario() {
+        System.out.println("Nombre: " + this.nombres + ", RUT: " + this.rut);
+    }
 }
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,9 +1,74 @@
-public class Usuario {
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
-String  nombre;
-int FechaDeNacimiento, Rut;
+public class Usuario implements Asesoria {
+    private String nombre;
+    private LocalDate fechaNacimiento;
+    private int run;
 
+    public Usuario() {
+    }
 
+    public Usuario(String nombre, LocalDate fechaNacimiento, int run) {
+        this.nombre = nombre;
+        this.fechaNacimiento = fechaNacimiento;
+        this.run = run;
+    }
 
+    public Usuario(String nombre2, String fechaNacimiento2, int run2) {
+    }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public int getRun() {
+        return run;
+    }
+
+    public void setRun(int run) {
+        this.run = run;
+    }
+
+    public int calcularEdad() {
+        LocalDate fechaActual = LocalDate.now();
+        int edad = fechaActual.getYear() - fechaNacimiento.getYear();
+        if (fechaNacimiento.getMonthValue() > fechaActual.getMonthValue() ||
+                (fechaNacimiento.getMonthValue() == fechaActual.getMonthValue() &&
+                        fechaNacimiento.getDayOfMonth() > fechaActual.getDayOfMonth())) {
+            edad--;
+        }
+        return edad;
+    }
+
+    public String mostrarEdad() {
+        int edad = calcularEdad();
+        return "El usuario tiene " + edad + " años";
+    }
+
+    public void analizarUsuario() {
+        System.out.println("Nombre: " + nombre + ", RUN: " + run);
+    }
+
+    @Override
+    public String toString() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        return "Usuario{" +
+                "nombre='" + nombre + '\'' +
+                ", fechaNacimiento=" + fechaNacimiento.format(formatter) +
+                ", run=" + run +
+                '}';
+    }
 }
